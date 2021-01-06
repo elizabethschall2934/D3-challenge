70e3e048c12d3ffd674a652d86d4f8d557b75fb5
elizabethschall2934 <elizabeth.schall2934@gmail.com>
2021-01-05 21:25:39 -0700

    getting scatter dots now, need to add scaling

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -7,15 +7,11 @@
 //Create and situate your axes and labels to the left and bottom of the chart.
 //Note: You'll need to use python -m http.server to run the visualization.
 
-// d3.csv("assets/data/data.csv").then((data) => {
-//     console.log(data);
-// });
-
 //https://www.d3-graph-gallery.com/graph/scatter_basic.html
 // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = {top: 20, right: 40, bottom: 60, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter")
@@ -27,18 +23,27 @@ var svg = d3.select("#scatter")
           "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv("assets/data/data.csv", function(data) {
+d3.csv("assets/data/data.csv").then(function(assetData) {
+//     console.log(assetData);
+
+// });
+
+// Cast the value to a number for each piece of data
+  assetData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+  });
 // Add X axis
 var x = d3.scaleLinear()
-.domain([0, 4000])
+.domain([0, 25])
 .range([ 0, width ]);
 svg.append("g")
-.attr("transform", "translate(0," + height + ")")
+.attr("transform", `translate(0, ${height})`)
 .call(d3.axisBottom(x));
 
 // Add Y axis
 var y = d3.scaleLinear()
-.domain([0, 500000])
+.domain([0, 25])
 .range([ height, 0]);
 svg.append("g")
 .call(d3.axisLeft(y));
@@ -46,12 +51,23 @@ svg.append("g")
 // Add dots
 svg.append('g')
 .selectAll("circle")
-.data(data)
+.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+.data(assetData)
 .enter()
 .append("circle")
-  .attr("cx", function (d) { return x(d.poverty); } )
-  .attr("cy", function (d) { return y(d.healthcare); } )
-  .attr("r", 1.5)
+  .attr("cx", data => (data.poverty))
+  .attr("cy", data => (data.healthcare))
+  .attr("r", 12)
   .style("fill", '#9f0fd8')
+
+// Add abbreviation text
+svg.append('g')
+.selectAll(null)
+    .data(assetData)
+    .enter()
+    .append("text")
+    .text(data => (data.abbr)) 
+    .attr("x", data => (data.poverty))
+    .attr("y", data => (data.healthcare))
 
 })
